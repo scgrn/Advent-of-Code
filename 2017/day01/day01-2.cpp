7275582c13bdb0a1ea7342f468e8f5e667b97d39
Andrew Krause <ajkrause@gmail.com>
2024-12-05 12:27:45 -0500

    Add solutions for day 1

---
+++ 2017/day01/day01-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char* argv[]) {
+    std::ifstream file("2017/day01/input.txt");
+    if (file.is_open()) {
+        std::string input;
+        getline(file, input);
+        file.close();
+
+        int total = 0;
+        for (int i = 0; i < input.length(); i++) {
+            int next = (i + (input.length() / 2)) % input.length();
+            if (input[i] == input[next]) {
+                total += input[next] - '0';
+            }
+        }
+        std::cout << total << std::endl;
+    } else {
+        std::cout << "Couldn't read input!" << std::endl;
+    }
+
+    return 0;
+}
+
