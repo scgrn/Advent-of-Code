fc5b6f53da98beae81a845cb8ff839175f9a1122
Andrew Krause <ajkrause@gmail.com>
2024-12-15 10:07:17 -0500

    Add solution for day 13 part 1

--- 2024/day13/day13-1.cpp
+++ 2024/day13/day13-1.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <regex>
 
 struct Vec2 {
     int x, y;
@@ -15,7 +16,7 @@ struct Machine {
 std::vector<Machine> machines;
 
 int solve(Machine machine) {
-    int det = machine.a.x * machine.b.y - machine.a.y* machine.b.x;
+    int det = machine.a.x * machine.b.y - machine.a.y * machine.b.x;
     int a = (machine.prize.x * machine.b.y - machine.prize.y * machine.b.x) / det;
     int b = (machine.a.x * machine.prize.y - machine.a.y * machine.prize.x) / det;
     if (machine.a.x * a + machine.b.x * b == machine.prize.x &&
@@ -32,20 +33,31 @@ int main(int argc, char* argv[]) {
     std::string line;
 
     if (file.is_open()) {
+        std::regex pattern(R"([0-9]+)");
+        std::smatch matches;
+        std::vector<int> soup;
+        
         while (getline(file, line)) {
+            auto begin = std::sregex_iterator(line.begin(), line.end(), pattern);
+            auto end = std::sregex_iterator();
+            
+            for (std::sregex_iterator i = begin; i != end; i++) {
+                std::smatch match = *i;
+                std::string matchStr = match.str();
+                soup.emplace_back(atoi(matchStr.c_str()));
+            }
         }
         file.close();
 
-        Machine m;
-        m.a = Vec2{94, 34};
-        m.b = Vec2{22, 67};
-        m.prize = Vec2{8400, 5400};
-        std::cout << solve(m) << std::endl;
-
-        m.a = Vec2{17, 86};
-        m.b = Vec2{84, 37};
-        m.prize = Vec2{7870, 6450};
-        std::cout << solve(m) << std::endl;
+        int total = 0;
+        for (int i = 0; i < soup.size() / 6; i++) {
+            Machine machine;
+            machine.a = Vec2{soup[i * 6 + 0], soup[i * 6 + 1]};
+            machine.b = Vec2{soup[i * 6 + 2], soup[i * 6 + 3]};
+            machine.prize = Vec2{soup[i * 6 + 4], soup[i * 6 + 5]};
+            total += solve(machine);
+        }
+        std::cout << total << std::endl;
     } else {
         std::cout << "Couldn't read input!" << std::endl;
     }
